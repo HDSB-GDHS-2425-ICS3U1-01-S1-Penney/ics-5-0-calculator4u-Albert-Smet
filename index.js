c5ea4b8cb7737108ad56d5c3016fd98e28e9c605
Albert <1smetanaalb@hdsb.ca>
2024-09-12 15:23:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-
-It is only adding fix it to also do other operations
-
 let num1 = prompt("What is the first value?");
 let num2 = prompt("What is the second value?");
 let sum = " ";
 let operation = prompt("What operation do you want to use (ie +, -, *, /)");
 
 function operationTypes(x, y) {
-    if (operation = "+") {
+    if (operation == "+") {
         sum = parseFloat(x)+parseFloat(y);
-    } else if (operation = "-") {
+    } else if (operation == "-") {
         sum = parseFloat(x)-parseFloat(y);
-    } else if (operation = "*") {
+    } else if (operation =="*") {
         sum = parseFloat(x)*parseFloat(y);
-    } else if (operation = "/") {
+    } else if (operation == "/") {
         sum = parseFloat(x)/parseFloat(y);
-    } else console.log("Equation is invalid, please enter different equation.");
+    } else console.log("Operation is invalid, please enter different opperation.");
 }
 
-operationTypes(num1, num2)
-console.log(parseFloat(sum))
+operationTypes(num1, num2);
+console.log(parseFloat(sum));
