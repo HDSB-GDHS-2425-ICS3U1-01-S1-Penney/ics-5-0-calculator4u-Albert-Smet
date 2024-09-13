2db6911f327e3f0a4e06d9ee0d88e33ca2297846
Albert <1smetanaalb@hdsb.ca>
2024-09-13 12:46:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+/*
+let operation = prompt("What operation do you want to use (ie +, -, *, /)");
 let num1 = prompt("What is the first value?");
 let num2 = prompt("What is the second value?");
 let sum = " ";
-let operation = prompt("What operation do you want to use (ie +, -, *, /)");
 
 function operationTypes(x, y) {
     if (operation == "+") {
@@ -16,4 +17,24 @@ function operationTypes(x, y) {
 }
 
 operationTypes(num1, num2);
-console.log(parseFloat(sum));
+
+function errorCodes() {
+    if (sum == Infinity) {
+        console.log("Cannot divide by zero.");
+    } else if (isNaN(sum)) {
+        console.log("Invalid input, plesae re-enter numbers.");
+    } else console.log(parseFloat(sum));
+}
+errorCodes();
+*/
+let equation = eval(prompt("Enter Your Equation"))
+
+function errorCodes() {
+    if (equation == Infinity) {
+        console.log("Cannot divide by zero.");
+    } else if (isNaN(equation)) {
+        console.log("Invalid input, plesae re-enter numbers.");
+    } else console.log(parseFloat(equation));
+}
+
+errorCodes();
